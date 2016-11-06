3db8838b98b78966e6505b1a598b9fe066ff5500
Mainak Chowdhury <mainakch@gmail.com>
2016-11-06 09:02:00 -0800

    server file for heroku deployment

--- server.js
+++ server.js
@@ -6,14 +6,62 @@ var tmclient = require('textmagic-rest-client');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // Body parser use JSON data
 
+// image files
 app.get('/checkmark.png', function(req, res) {
     res.sendFile(__dirname + '/checkmark.png');
 });
 
+app.get('/check-xray.jpg', function(req, res) {
+    res.sendFile(__dirname + '/chest-xray.jpg');
+});
+
+app.get('/patient-icon.jpg', function(req, res) {
+    res.sendFile(__dirname + '/patient-icon.jpg');
+});
+
+app.get('/elderly-woman.png', function(req, res) {
+    res.sendFile(__dirname + '/elderly-woman.png');
+});
+
+app.get('/check.png', function(req, res) {
+    res.sendFile(__dirname + '/check.png');
+});
+
+app.get('/patient_icon.jpg', function(req, res) {
+    res.sendFile(__dirname + '/patient_icon.jpg');
+});
+
+// html files
+app.get('/confirm.html', function(req, res) {
+    res.sendFile(__dirname + '/confirm.html');
+});
+
+app.get('/tx2.html', function(req, res) {
+    res.sendFile(__dirname + '/tx2.html');
+});
+
+app.get('/tx2.css', function(req, res) {
+    res.sendFile(__dirname + '/tx2.css');
+});
+
+app.get('/receiving.html', function(req, res) {
+    res.sendFile(__dirname + '/receiving.html');
+});
+
+// css files
 app.get('/tx.css', function(req, res) {
     res.sendFile(__dirname + '/tx.css');
 });
 
+app.get('/receiving.css', function(req, res) {
+    res.sendFile(__dirname + '/receiving.css');
+});
+
+app.get('/bootstrap.css', function(req, res) {
+    res.sendFile(__dirname + '/bootstrap.css');
+});
+
+// js files
 app.get('/tx.js', function(req, res) {
     res.sendFile(__dirname + '/tx.js');
 });
@@ -22,44 +70,25 @@ app.get('/receiving.js', function(req, res) {
     res.sendFile(__dirname + '/receiving.js');
 });
 
-app.get('/receiving.css', function(req, res) {
-    res.sendFile(__dirname + '/receiving.css');
-});
-
-app.get('/patient_icon.jpg', function(req, res) {
-    res.sendFile(__dirname + '/patient_icon.jpg');
-});
-
-app.get('/bootstrap.css', function(req, res) {
-    res.sendFile(__dirname + '/bootstrap.css');
-});
-
 app.get('/submit.js', function(req, res) {
     res.sendFile(__dirname + '/submit.js');
 });
 
+
+// default route
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/tx.html');
+    res.sendFile(__dirname + '/tx2.html');
 });
 
-app.get('/tx.html', function(req, res) {
-    res.sendFile(__dirname + '/tx.html');
-});
 
-app.get('/rx.html', function(req, res) {
-    res.sendFile(__dirname + '/rx.html');
-});
 
 app.post('/api', function(req, res) {
-    console.log(req);
-    console.log(req.body);
     var patientid = req.body.id;
-    url = req.protocol + '://' + req.get('host') + '/reception' + '?id=' + patientid;
+    url = req.protocol + '://' + req.get('host') + '/receiving.html';
     var c = new tmclient('mainakchowdhury', 'DgqCit7iyRXCAnb0A6niGthgLQgLuU');
-    c.Messages.send({text: url, phones:'16502384531'}, function(err, res){
+    c.Messages.send({text: url, phones:'19492943766'}, function(err, res){
        console.log('Messages.send()', err, res);
     });
-    console.log(url);
     res.send(url);
 });
 
@@ -69,5 +98,5 @@ app.get('/reception', function(req, res) {
     console.log(id);
 });
 
-app_new = app.listen(process.env.PORT);
+app_new = app.listen(process.env.PORT||3772);
 //console.log("Server Running on ", PORT);
