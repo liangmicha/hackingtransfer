319cdc8f9afc3f115b418d33b8b2ad33c32914d7
Michael Liang <liangmicha@gmail.com>
2016-11-05 18:17:30 -0700

    Adding recevieng content

--- receiving.js
+++ receiving.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  console.log("blah");
+
+  $('.discharge-summary-wrapper').click(function() {
+    $('.discharge-summary-text').show();
+  });
+
+  $('.labs-wrapper').click(function() {
+    $('.labs-graph').show();
+  });
+
 });
