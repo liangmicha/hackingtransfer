c909237aad1380fc4652ca2ff83fd7ff118b0847
mguo <mguo@mit.edu>
2016-11-05 19:47:57 -0700

    transfer update

--- tx.js
+++ tx.js
@@ -1,31 +1,37 @@
 $(document).ready(function() {
   console.log("blah");
+
+
+    $('.import_ehr').click(function() {
+    $('.dc_check_image').show();
+
+
   	function dc_summary_check() {
-    document.getElementById("dc_summary").checked = true;
+    document.getElementById("dc_summary_box").checked = true;
 	}
 
 	function dc_summary_uncheck() {
-	    document.getElementById("dc_summary").checked = false;
+	    document.getElementById("dc_summary_box").checked = false;
 		}
 
 	function labs_check() {
-    document.getElementById("labs").checked = true;
+    document.getElementById("labs_box").checked = true;
 	}
 
 	function labs_uncheck() {
-	    document.getElementById("labs").checked = false;
+	    document.getElementById("labs_box").checked = false;
 		}
 
 	function images_check() {
-    document.getElementById("images").checked = true;
+    document.getElementById("images_box").checked = true;
 	}
 
 	function images_uncheck() {
-	    document.getElementById("images").checked = false;
+	    document.getElementById("images_box").checked = false;
 		}
 
 	function medication_check() {
-    document.getElementById("medication").checked = true;
+    document.getElementById("medication_box").checked = true;
 	}
 
 	function medication_uncheck() {
