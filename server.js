cf9a20be2b44e33b02d16a97a0c55ade2f33fa58
Mainak Chowdhury <mainakch@gmail.com>
2016-12-23 12:57:01 -0800

    added timeout boilerplate

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // Body parser use JSON data
 
 var globalhash = {};
+var globaltimestamp = {};
+var c = new tmclient(process.env.TXTUSERNAME, process.env.TXTPASSWORD);
+
+function delete_old_records () {
+    var time_cutoff = Math.floor(Date.now() / 1000) - 120;
+    // two mins cutoff
+    Object.keys(globalhash).forEach(function(key) {
+      if (globaltimestamp[key] < time_cutoff) {
+          delete globaltimestamp[key];
+          delete globalhash[key];
+      }
+    });
+}
 
 // image files
 app.get('/checkmark.png', function(req, res) {
@@ -48,13 +61,12 @@ app.get('/tx2.css', function(req, res) {
 
 app.get('/receiving.html', function(req, res) {
         var num = globalhash[req.query.hash];
-        var c = new tmclient('anjalidatta', 'j78hZTKazcpoJPbCb4JtLsHJwd6Yh2');
-        c.Messages.send({text: 'Record ' + req.query.hash + ' accessed', phones:num}, function(err, res){
-           console.log('Messages.send()', err, res);
-        });
+	if (process.env.TXT_NOTIFY=='1') {
+            c.Messages.send({text: 'Record ' + req.query.hash + ' accessed', phones:num}, function(err, res){
+               console.log('Messages.send()', err, res);
+            });
+	}
  
-        console.log('Deleting' + req.query.hash);
-        delete globalhash[req.query.hash];
         console.log(globalhash);
         res.sendFile(__dirname + '/receiving.html');
 
@@ -93,19 +105,25 @@ app.get('/', function(req, res) {
 });
 
 app.post('/api', function(req, res) {
-    console.log("Here I am\n");
+    console.log("Inside api\n");
     console.log(req.body.id);
     var rnd = Math.floor(Math.random()*100000000);
     console.log(rnd, " ", req.body.id);
     globalhash[rnd] = req.body.id;
+    globaltimestamp[rnd] = Math.floor(Date.now() / 1000);
 
     url = req.protocol + '://' + req.get('host') + '/receiving.html?hash=' + rnd.toString();
-    var c = new tmclient('anjalidatta', 'j78hZTKazcpoJPbCb4JtLsHJwd6Yh2');
     console.log(url);
-    c.Messages.send({text: url, phones:'19492943766'}, function(err, res){
-    //c.Messages.send({text: url, phones:'18172699548'}, function(err, res){
-       console.log('Messages.send()', err, res);
-    });
+    console.log(globaltimestamp[rnd], globaltimestamp[rnd] + 1);
+
+    var number = process.env.DEBUG_PHONE;
+    if (process.env.DEBUG == '0') number = process.env.PRODUCTION_PHONE;
+    if (process.env.TXT_NOTIFY == '1') {
+        c.Messages.send({text: url, phones:number}, function(err, res){
+           console.log('Messages.send()', err, res);
+        });
+    }
+
     res.send("Hello");
 });
 
